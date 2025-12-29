afc513e017bc89edb6b45484a3c36728228eabc3
tirateun <diego.borjas@upch.pe>
2025-12-29 12:04:26 -0500

    fix: usar bootstrap-icons desde CDN para Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import App from "./App";
 import "./index.css";
 import "./theme-unmsm.css"; // 🆕 Tema institucional UNMSM
 
-// Importar Bootstrap Icons (si aún no lo tienes)
-import "bootstrap-icons/font/bootstrap-icons.css";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
